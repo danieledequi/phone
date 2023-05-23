8804b1cbceaf0c70b1c63166dd77d43e31b5d811
Daniele Dequi <113129822+danieledequi@users.noreply.github.com>
2023-05-23 16:09:47 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-const listaDeTeclas = document.querySelectorAll('input[type=button]');
-const inputTel = document.querySelector('input[type=tel]');
+const listaDeTeclas = document.querySelectorAll("input[type=button]");
+const inputTel = document.querySelector("input[type=tel]");
 
 for (let indice = 0; indice < listaDeTeclas.length;indice++) {
-   
-    const tecla = listaDeTeclas[indice];
+       const tecla = listaDeTeclas[indice];
 
-    tecla.onkeydown = function(evento) {
-        if(evento.code === "Enter" || evento.code === "Space") {
-            tecla.classList.add('ativa');
+    tecla.onclick = () => {
+        inputTel.value += tecla.value;
+    };
+
+    tecla.onkeydown = function (evento) {
+        if (evento.code === "Enter" || evento.code === "Space") {
+            tecla.classList.add("ativa");
         }
-    }
+    };
 
     tecla.onkeyup = function () {
-        tecla.classList.remove('ativa');
-    }
+        tecla.classList.remove("ativa");
+    };
+}
+
+function limparCampos() {
+    document.getElementsByClassName("digitar")[0].value = "";
 }
